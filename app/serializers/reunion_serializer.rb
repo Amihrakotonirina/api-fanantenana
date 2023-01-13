0ac6ce7e15a09bba6e5e5612670e59542f6cfff6
Mirana RAKOTONIRINA <mihrakotonirina@gmail.com>
2023-01-13 22:06:16 +0300

    serializer

--- app/serializers/reunion_serializer.rb
+++ app/serializers/reunion_serializer.rb
@@ -1,4 +1,5 @@
 class ReunionSerializer < ActiveModel::Serializer
   attributes :id, :dateReunion, :heureDebut, :heureFin, :ouverte, :commentaire
   has_one :categorie_reunion
+  has_many :presences
 end
