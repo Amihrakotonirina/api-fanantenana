9a00b9498bd1c5e0b023dfa8c04851b50b3b88e4
Mirana RAKOTONIRINA <mihrakotonirina@gmail.com>
2023-01-14 15:34:44 +0300

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     #origins 'http://localhost:8080'
-    origins 'https://api-fanantenana.onrender.com'
+    origins 'https://sampana-fanantenana.netlify.app'
 
     resource '*',
       headers: :any,
